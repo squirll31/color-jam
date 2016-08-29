a2e5b48f5168c3bccdecec3734b2aca37e9b77e4
Matthew Wyble <poobqbeinc@gmail.com>
2016-08-28 21:00:21 -0700

    added code.

---
+++ js/colorlist.js
@@ -0,0 +1,66 @@
+var COLOR_OBJ = {
+	"white" : {
+		id : "0",
+		color : "#FFFFFF"
+	},
+	"silver" : {
+		id : "1",
+		color : "#C0C0C0"
+	},
+	"gray" : {
+		id : "2",
+		color : "#808080"
+	},
+	"black" : {
+		id : "3",
+		color : "#000000"
+	},
+	"red" : {
+		id : "4",
+		color : "#FF0000"
+	},
+	"maroon" : {
+		id : "5",
+		color : "#800000"
+	},
+	"yellow" : {
+		id : "6",
+		color : "#FFFF00"
+	},
+	"olive" : {
+		id : "7",
+		color : "#808000"
+	},
+	"lime" : {
+		id : "8",
+		color : "#00FF00"
+	},
+	"green" : {
+		id : "9",
+		color : "#008000"
+	},
+	"aqua" : {
+		id : "10",
+		color : "#00FFFF"
+	},
+	"teal" : {
+		id : "11",
+		color : "#008080"
+	},
+	"blue" : {
+		id : "12",
+		color : "#0000FF"
+	},
+	"navy" : {
+		id : "13",
+		color : "#000080"
+	},
+	"fuschia" : {
+		id : "14",
+		color : "#FF00FF"
+	},
+	"purple" : {
+		id : "15",
+		color : "800080"
+	}
+};
